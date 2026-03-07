8d2a783389a9e85022a958bb7355fb1d06bc17a6
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-03-07 07:56:58 +0100

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router";
+import { AuthProvider } from "./app/lib/contexts/AuthContext";
+import { router } from "./app/routes";
+import "./styles/index.css";
+
+createRoot(document.getElementById("root")!).render(
+  <AuthProvider>
+    <RouterProvider router={router} />
+  </AuthProvider>
+);
+
