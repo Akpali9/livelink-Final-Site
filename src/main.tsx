1fa97e94c1ddc5275599ca023b43229c5e95d207
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-03-09 11:26:54 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./app/App.tsx";
+import { RouterProvider } from "react-router";
+import { router } from "./app/routes";
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
