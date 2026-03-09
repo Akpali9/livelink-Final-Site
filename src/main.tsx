1eab350c34fd68c6801c883c97582657c821c46c
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-03-10 00:14:48 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router";
+import { RouterProvider } from "react-router-dom";
 import { router } from "./app/routes";
 import "./styles/index.css";
 
