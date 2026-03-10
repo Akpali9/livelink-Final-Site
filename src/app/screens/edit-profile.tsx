84caeb0bdb4ed62bbf72555d24ae791fbc655731
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-03-10 12:39:10 +0100

    Update edit-profile.tsx

--- src/app/screens/edit-profile.tsx
+++ src/app/screens/edit-profile.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router";
 import { CheckCircle2, MapPin, Plus, X, Save, Upload, Loader2, Zap } from "lucide-react";
 import { motion, AnimatePresence } from "motion/react";
 import { AppHeader } from "../components/app-header";
