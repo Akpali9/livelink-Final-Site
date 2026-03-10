9c5f8db5671295f5237c5a41fd1acd5beae28a31
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-03-10 12:35:04 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import { router } from "./app/routes";
+import { AuthProvider } from "./app/lib/contexts/AuthContext";
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />
+  <AuthProvider>
+    <RouterProvider router={router} />
+  </AuthProvider>
 );
