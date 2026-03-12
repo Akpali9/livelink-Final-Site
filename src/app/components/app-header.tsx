f9591668d813ae4a5a129c4f0c27a26a9cd7d251
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-03-12 02:47:05 -0700

    Update app-header.tsx

--- src/app/components/app-header.tsx
+++ src/app/components/app-header.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Link, useNavigate } from "react-router";
+import { Link, useNavigate, useLocation } from "react-router";
 import {
   MessageSquare,
   Bell,
