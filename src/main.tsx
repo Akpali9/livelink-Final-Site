ab55234cba09fe3803259fc6fce355e41f8b3273
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-03-17 01:16:39 +0800

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router';
-import App from './app/App';
-import './styles/index.css';
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router";
+import { AuthProvider } from "./app/lib/contexts/AuthContext";
+import { router } from "./app/routes";
+import "./styles/index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <AuthProvider>
+    <RouterProvider router={router} />
+  </AuthProvider>
 );
+
