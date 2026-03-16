5f34cd18eeb1d2f5c61c95b99b212aafc2f480c9
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-03-16 17:06:06 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router";
-import { AuthProvider } from "./app/lib/contexts/AuthContext";
-import { router } from "./app/routes";
-import "./styles/index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './styles/index.css';
 
-createRoot(document.getElementById("root")!).render(
-  <AuthProvider>  
-    <RouterProvider router={router} />
-  </AuthProvider>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
