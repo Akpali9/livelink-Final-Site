0a249f9ef2e6f6e48de780eca4e40d71abff8612
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-03-19 18:20:50 +0800

    Update AdminDashboard.tsx

--- src/app/components/AdminDashboard.tsx
+++ src/app/components/AdminDashboard.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router";
-import { AdminBusinessQueue } from './screen/AdminBusinessQueue';
+import { AdminBusinessQueue } from './AdminBusinessQueue';
 {activeTab === "businesses" && (
   <AdminBusinessQueue />
 )}
