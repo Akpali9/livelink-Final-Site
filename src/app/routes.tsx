2452ac76038fda5321817e2d3168017be545b4e2
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-03-21 14:49:20 +0100

    Update routes.tsx

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -6,9 +6,9 @@ import { Campaigns } from "./screens/campaigns";
 import { BusinessDashboard } from "./screens/business-dashboard";
 import { BusinessProfile } from "./screens/business-profile";
 import { BusinessSettings } from "./screens/business-settings";
-// import { BusinessCampaigns } from "./screens/business-campaigns";
+import { BusinessCampaigns } from "./screens/business-campaign-detail";
 import { BusinessCampaignDetail } from "./screens/business-campaign-detail";
-// import { BusinessCampaignCreators } from "./screens/business-campaign-creators";
+import { BusinessCampaignCreators } from "./screens/business-campaign-detail";
 import { BrowseBusinesses } from "./screens/browse-businesses";
 import { BrowseCreators } from "./screens/browse";
 import { Messages } from "./screens/messages"; // Commented out - file doesn't exist
