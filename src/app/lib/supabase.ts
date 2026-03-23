ec0e677ad3608aa4c5c7b30fc983491fb6608235
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-03-23 18:58:50 +0800

    Update supabase.ts

--- src/app/lib/supabase.ts
+++ src/app/lib/supabase.ts
@@ -1,4 +1,3 @@
-
 import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL as string;
@@ -10,4 +9,10 @@ if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
   );
 }
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+  },
+});
