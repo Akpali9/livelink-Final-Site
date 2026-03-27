3b0777a747022c09b7e406d8cb6785d38f1f935d
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-03-28 00:05:04 +0800

    Update index.ts

--- src/app/screens/supabase/functions/insert-proof/index.ts
+++ src/app/screens/supabase/functions/insert-proof/index.ts
@@ -3,7 +3,7 @@ import { createClient } from 'jsr:@supabase/supabase-js@2'
 Deno.serve(async (req) => {
   const supabase = createClient(
     Deno.env.get('SUPABASE_URL')!,
-    Deno.env.get('SUPABASE_SERVICE_ROLE_KEY')!
+    Deno.env.get('SUPABASE_SERVICE_ROLE_KEY')!   // bypass RLS
   );
 
   const body = await req.json();
