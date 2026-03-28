ddef994b1fb5d200bf3611c3c9e0390cb8841ed6
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-03-28 22:28:46 +0100

    Update main.tsx

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,10 +3,11 @@ import { RouterProvider } from "react-router";
 import { AuthProvider } from "./app/lib/contexts/AuthContext";
 import { router } from "./app/routes";
 import "./styles/index.css";
+import { Toaster } from "sonner"; // <-- add this
 
 createRoot(document.getElementById("root")!).render(
   <AuthProvider>
+    <Toaster richColors position="top-center" />
     <RouterProvider router={router} />
   </AuthProvider>
 );
-
